f4f51fc3f1c89c8538471911dd4cd32e91610e21
Ejko <liliya.stefanova@gmail.com>
2013-10-20 10:55:55 +0100

    Program which takes a number in a non parsed format, e.g. 23,456.78 and converts it to decimal and displays the number which is half of it. Conversion takes place without the use of parseDouble() method

--- palindrome.groovy
+++ palindrome.groovy
@@ -1,17 +1,16 @@
 //detect if a text input by the user is a palindrome
 String str, palindromeCheck, letter
 print "Please enter text to check if palindrome: "
-str=new String(System.console().readLine())
-palindromeCheck=new String(str.substring(0, 1))
+str = new String(System.console().readLine())
+palindromeCheck = new String()
 
-for(i=str.length(); i>0; i--) {
-	char c=str.charAt(i)
-	letter=Character.toString(c)
-	palindromeCheck=palindromeCheck+letter
-	}
-	if (palindromeCheck.equals(str)) {
-	print "The text you entered is a palindrome"
-	}
-else{
-	print "The text you entered is not a palindrome, try again"
-	}	
+for (i = str.length() - 1; i >= 0; i--) {
+    char c = str.charAt(i)
+    letter = Character.toString(c)
+    palindromeCheck = palindromeCheck + letter
+}
+if (palindromeCheck.equals(str)) {
+    println "The text you entered is a palindrome"
+} else {
+    println "The text you entered is not a palindrome, try again"
+}
