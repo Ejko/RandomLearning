a0fdef5838fbda8b49bed649a1e320ee3152003d
Ejko <liliya.stefanova@gmail.com>
2013-10-24 22:11:54 +0100

    Program which has a class with 2 methods: The first method takes a binary number and converts it to decimal; this method takes in String and returns int The second method takes a decimal number and converts it to binary ; this method takes in int and returns String There is a menu for the user to choose which option they require

--- binaryDecimal.groovy
+++ binaryDecimal.groovy
@@ -1,31 +1,36 @@
 //converts binary to decimal numbers as specified by the user
 class Conversion {
-	String binaryNum
-	int decimalNum
-	
-	int binary2decimal(){
-		int powerPos=binaryNum.length()
-		int binNum=0
-		for(int i=0; i<binaryNum.length(); i++){
-			char c=binaryNum.charAt(i)
-			println  "The current char is:" + c
-			int n=Integer.parseInt(c)*Math.pow(2,powerPos)
-			println "The current converted position is: "+ n
-			powerPos--
-			binNum=binNum+n
+
+	int binary2decimal(String binaryNum){
+		int powerPos=binaryNum.length()-1   //holds the current position of the binary number
+		int decNum=0
+		for(int i=0; i<binaryNum.length(); i++){  //loops through the binary number
+			String c=binaryNum.charAt(i)
+			int n=Integer.parseInt(c)* Math.pow(2, powerPos) //converts the current binary position to decimal
+			powerPos--                                       //the position is reduced by one to reflect the next binary
+			decNum=decNum+n                                  //digit position
 		}
-		return binNum	
+		return decNum                                       //the sum of all individually converted binary digits
 	}
-	//String decimal2binary(int dec){
-	//	dec=decimalNum
-	//	int nextDivision=decimalNum
-	//	while(nextDivision!=0){
-		//	nextDivision=nextDivision/2
-		//	int modDiv=nextDivision%2
-		//	String binary=binary+String.parseString(modDiv)
-		//}
-		//return binary 
-	//}
+	String decimal2binary( int decimalNum){
+	    String binNum=new String()
+        String backBinNum=new String()
+        int nextDivision=decimalNum
+        while(nextDivision!=0){
+            int modDiv=nextDivision%2                       //calculates the current modulo
+			nextDivision=nextDivision/2                     //finds the next quotient by dividing by 2
+            println "The quotient is: "+ nextDivision
+			println "The modulo is: "+modDiv
+            //backBinNum only stores the collection of binary position, but they are in incorrect order
+            backBinNum=backBinNum+Integer.toString(modDiv)  //the modulo is continously added to a string
+            }
+        //loops through backBinNum to swap the reverse the position of binary digits and reflect the bin number correctly
+        for(int i=backBinNum.length(); i>0;i--){
+        char c=backBinNum.charAt(i-1)
+         binNum=binNum+c
+        }
+		return binNum
+	}
 }
 Conversion numericSystem=new Conversion()
 println "Please type in the option you prefer:"
@@ -35,15 +40,14 @@ String input=System.console().readLine()
 switch(input){
 	case "1":
 		print "Please enter a binary number to convert: "
-		numericSystem.binaryNum=System.console().readLine()
-		//String str=System.console().readLine()
-		println "The number entered: " + numericSystem.binaryNum
-		println "The decimal number is: " + numericSystem.binary2decimal()
+		String binNum=System.console().readLine()
+		println "The number entered: " + binNum
+		println "The decimal number is: " + numericSystem.binary2decimal(binNum)
 		break
 	case "2":
 		print "Please enter a decimal number to convert: "
-		numericSystem.decimalNum=Integer.parseInt(System.console().readLine())
-		println "The binary number is: " + numericSystem.decimal2binary()
+        int decNum=Integer.parseInt(System.console().readLine())
+		println "The binary number is: " + numericSystem.decimal2binary(decNum)
 		break
 	default:
 		print "You have selected an invalid option!"
